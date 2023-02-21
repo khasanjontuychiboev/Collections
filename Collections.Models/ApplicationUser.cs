65e73ddf5dddcc29ef0ae34090b8f4d366f8020e
Xasanboy <ucbeyibenim@gmail.com>
2023-02-21 10:54:32 +0500

    Application User that derived from identity user was created and ApplicationDBContext was also created. EntityFrameWork added.

---
+++ Collections.Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+    }
+}
